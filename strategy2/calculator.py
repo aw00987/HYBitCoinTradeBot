e6ebfb3350fdd05282089a1f5dce4f90057a2242
wbt <bt01520840@gmail.com>
2024-12-13 15:16:40 +0800

    feat：去除相同重复信号

--- strategy2/calculator.py
+++ strategy2/calculator.py
@@ -19,6 +19,31 @@ def calculate_tema_signal(df, zero_threshold, second_derivative_threshold):
         'Signal'
     ] = -1
 
+    # 去除相同的重复信号
+
+    # 提前确保signal列为int类型（或至少是可比较类型）
+    df['Signal'] = df['Signal'].astype(int)
+
+    last_non_zero = 0  # 用于记录上一个非0信号
+    adjusted_signals = []  # 存储调整后的信号
+
+    for s in df['Signal']:
+        if s == 0:
+            # 当前信号为0则无需特别处理，直接加入结果
+            adjusted_signals.append(s)
+        else:
+            # 当前信号为非0
+            if s == last_non_zero:
+                # 与上一个非0信号相同，需要忽略（变成0）
+                adjusted_signals.append(0)
+            else:
+                # 不同非0信号，则更新last_non_zero并保留该信号
+                adjusted_signals.append(s)
+                last_non_zero = s
+
+    # 将调整后的信号回写到DataFrame
+    df['Signal'] = adjusted_signals
+
 def calculate_alligator_signal(df):
     df['jaw'] = talib.SMA(df['close'], 13)
     df['teeth'] = talib.SMA(df['close'], 8)
